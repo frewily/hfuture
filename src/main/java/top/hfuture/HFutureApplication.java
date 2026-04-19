3f695ebe33a110fdd2c2b07514d5d61530235882
frewily <2994508531@qq.com>
2026-04-19 18:31:43 +0800

    项目初始化

---
+++ src/main/java/top/hfuture/HFutureApplication.java
@@ -0,0 +1,13 @@
+package top.hfuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HFutureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HFutureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/hfuture/HFutureApplicationTests.java
@@ -0,0 +1,13 @@
+package top.hfuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HFutureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
